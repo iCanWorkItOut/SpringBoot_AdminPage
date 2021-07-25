6991c8ab5c6b645685edafec321c06c671d3836f
Sang-hyun Seo <icanworkitout@daum.net>
2021-07-25 12:17:29 +0900

    Controller 추상화 적용하기

--- src/main/java/com/example/study/controller/api/ItemApiController.java
+++ src/main/java/com/example/study/controller/api/ItemApiController.java
@@ -1,6 +1,7 @@
 package com.example.study.controller.api;
 
 
+import com.example.study.controller.CrudController;
 import com.example.study.ifs.CrudInterface;
 import com.example.study.model.network.Header;
 import com.example.study.model.network.request.ItemApiRequest;
@@ -9,34 +10,41 @@ import com.example.study.service.ItemApiLogicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/api/item")
-public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+public class ItemApiController extends CrudController<ItemApiRequest, ItemApiResponse> {
 
     @Autowired
     private ItemApiLogicService itemApiLogicService;
 
-    @Override
-    @PostMapping("") // /api/item
-    public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> request) {
-        return itemApiLogicService.create(request);
+    @PostConstruct // 초기생성자와 같이 동작
+    public void init() {
+        this.baseService = itemApiLogicService;
     }
-
-    @Override
-    @GetMapping("{id}") // /api/item/{id}
-    public Header<ItemApiResponse> read(@PathVariable long id) {
-        return itemApiLogicService.read(id);
-    }
-
-    @Override
-    @PutMapping("") // /api/item
-    public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> request) {
-        return itemApiLogicService.update(request);
-    }
-
-    @Override
-    @DeleteMapping("{id}") // /api/item/{id}
-    public Header delete(@PathVariable long id) {
-        return itemApiLogicService.delete(id);
-    }
+// CrudController 추상클래스 상속 받음으로써 생략 가능
+//    @Override
+//    @PostMapping("") // /api/item
+//    public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> request) {
+//        return itemApiLogicService.create(request);
+//    }
+//
+//    @Override
+//    @GetMapping("{id}") // /api/item/{id}
+//    public Header<ItemApiResponse> read(@PathVariable long id) {
+//        return itemApiLogicService.read(id);
+//    }
+//
+//    @Override
+//    @PutMapping("") // /api/item
+//    public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> request) {
+//        return itemApiLogicService.update(request);
+//    }
+//
+//    @Override
+//    @DeleteMapping("{id}") // /api/item/{id}
+//    public Header delete(@PathVariable long id) {
+//        return itemApiLogicService.delete(id);
+//    }
 }
--- src/main/java/com/example/study/controller/api/UserApiController.java
+++ src/main/java/com/example/study/controller/api/UserApiController.java
@@ -1,5 +1,6 @@
 package com.example.study.controller.api;
 
+import com.example.study.controller.CrudController;
 import com.example.study.ifs.CrudInterface;
 import com.example.study.model.network.Header;
 import com.example.study.model.network.request.UserApiRequest;
@@ -9,38 +10,45 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+
 @Slf4j // Simple Logging Facade for Java
 @RestController
 @RequestMapping("/api/user")
-public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
+public class UserApiController extends CrudController<UserApiRequest, UserApiResponse> {
 
     @Autowired
     private UserApiLogicService userApiLogicService;
 
-    @Override
-    @PostMapping("") // /api/user
-    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> request) {
-        log.info("{}", request); // Slf4j
-        return userApiLogicService.create(request);
+    @PostConstruct
+    public void init() {
+        this.baseService = userApiLogicService;
     }
 
-    @Override
-    @GetMapping("{id}") // /api/user/{id}
-   public Header<UserApiResponse> read(@PathVariable(name = "id") long id) { // {id} 변수명이 파라미터 id명과 같을 경우 (name = ) 생략 가능
-        log.info("read id : {}", id);
-        return userApiLogicService.read(id);
-    }
-
-    @Override
-    @PutMapping("") // /api/user
-    public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> request) {
-        return userApiLogicService.update(request);
-    }
-
-    @Override
-    @DeleteMapping("{id}") // /api/user/{id}
-    public Header delete(@PathVariable long id) {
-        log.info("delete : {}", id);
-        return userApiLogicService.delete(id);
-    }
+//    @Override
+//    @PostMapping("") // /api/user
+//    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> request) {
+//        log.info("{}", request); // Slf4j
+//        return userApiLogicService.create(request);
+//    }
+//
+//    @Override
+//    @GetMapping("{id}") // /api/user/{id}
+//   public Header<UserApiResponse> read(@PathVariable(name = "id") long id) { // {id} 변수명이 파라미터 id명과 같을 경우 (name = ) 생략 가능
+//        log.info("read id : {}", id);
+//        return userApiLogicService.read(id);
+//    }
+//
+//    @Override
+//    @PutMapping("") // /api/user
+//    public Header<UserApiResponse> update(@RequestBody Header<UserApiRequest> request) {
+//        return userApiLogicService.update(request);
+//    }
+//
+//    @Override
+//    @DeleteMapping("{id}") // /api/user/{id}
+//    public Header delete(@PathVariable long id) {
+//        log.info("delete : {}", id);
+//        return userApiLogicService.delete(id);
+//    }
 }
